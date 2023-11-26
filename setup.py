280557514e7aca09062c3955bf8d8ad8f236779f
eli <eli@f>
2023-11-26 15:30:20 +0200

    fix bug

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='gpt',
-    version='1.2.1',
+    version='1.2.2',
     packages=['gpt'],
     install_requires=requirements,
     entry_points={
